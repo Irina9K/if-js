74c1d895621375d06eeb4d6d02807c639119d714
Irina Kulai <87722296+Irina9K@users.noreply.github.com>
2022-05-05 10:52:35 +0300

    fixed inf

--- main.js
+++ main.js
@@ -46,60 +46,85 @@ for (let i = 0; i < arr3.length; i++) {
 // Functions - part 1
 // Palindrome function
 console.log('function palindrome');
+
 function palindrome(word) {
+
   let arrWord = word.split('');
-  for (let i = 0; i < arrWord.length; i++) {
-    if (arrWord[i] === arrWord[arrWord.length - ++i]) {
+  let lengthWord = arrWord.length;
+  let halfLengthWord = Math.floor(lengthWord / 2);
+
+  for (let i = 0; i < halfLengthWord; i++) {
+
+    if (arrWord[i] === arrWord[lengthWord - ++i]) {
       console.log(true);
-    } else console.log(false);
+    } else {
+      console.log(false);
+    }
+
   }
+
 }
 palindrome('шалаш');
 
 // function min(a, b)
 console.log('function min(a, b)');
+
 function min1(a, b) {
+
   if (a < b) {
     console.log(a);
   } else {
     console.log(b);
   }
+
 }
 min1(15, 6);
 
 function min2(a, b) {
+
   a < b ? console.log(a) : console.log(b);
+
 }
 min2(15, 6);
 
 // function max(a, b)
 console.log('function max(a, b)');
+
 function max1(a, b) {
+
   if (a > b) {
     console.log(a);
   } else {
     console.log(b);
   }
+
 }
 max1(15, 6);
 
 function max2(a, b) {
+
   a > b ? console.log(a) : console.log(b);
+
 }
 max2(15, 6);
 
 // Replacing array elements
 
 console.log('Generated array');
+
 let elements = [];
+
 for (let i = 0; i < 10; i++) {
   elements[i] = Math.floor(Math.random() * 101);
   console.log(elements[i]);
 }
 
 console.log('Converted array');
-function replacingElements() {
+
+function replacingElements(elements) {
+
   for (let i = 0; i < elements.length; i++) {
+
     if (elements[i] === 0) {
       console.log('zero');
     } else if (elements[i] % 100 === 0) {
@@ -109,6 +134,7 @@ function replacingElements() {
     } else {
       console.log(elements[i]);
     }
+
   }
 }
 replacingElements(elements);
