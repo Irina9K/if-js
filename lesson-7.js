6ddd396e0b4367f81d4b86b76128523fa6fabb56
Irina Kulai <87722296+Irina9K@users.noreply.github.com>
2022-05-21 18:03:05 +0300

    added task 7

--- lesson-7.js
+++ lesson-7.js
@@ -1,38 +1,56 @@
-// const obj1 = {
-//   a: 'a',
-//   b: {
-//     a: 'a',
-//     b: 'b',
-//     c: {
-//       a: 1,
-//     },
-//   },
-// };
-// const obj2 = {
-//   b: {
-//     c: {
-//       a: 1,
-//     },
-//     b: 'b',
-//     a: 'a',
-//   },
-//   a: 'a',
-// };
-// const obj3 = {
-//   a: {
-//     c: {
-//       a: 'a',
-//     },
-//     b: 'b',
-//     a: 'a',
-//   },
-//   b: 'b',
-// };
-//
-// const deepEqual = (object1, object2) => {
-//   console.log(Object.entries(obj1));
-// };
-//
-// // deepEqual(obj1, obj2); // true
-// // deepEqual(obj1, obj3); // false
-// deepEqual(obj1);
+const obj1 = {
+  a: 'a',
+  b: {
+    a: 'a',
+    b: 'b',
+    c: {
+      a: 1,
+    },
+  },
+};
+const obj2 = {
+  b: {
+    c: {
+      a: 1,
+    },
+    b: 'b',
+    a: 'a',
+  },
+  a: 'a',
+};
+const obj3 = {
+  a: {
+    c: {
+      a: 'a',
+    },
+    b: 'b',
+    a: 'a',
+  },
+  b: 'b',
+};
+
+const deepEqual = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keys1.length; i += 1) {
+    const key = keys1[i];
+    const typeKey = typeof object1[key] === 'object' && typeof (object2[key] === 'object');
+
+    if (
+      (!typeKey && object1[key] !== object2[key]) ||
+      (typeKey && !deepEqual(object1[key], object2[key]))
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(deepEqual(obj1, obj2)); // true
+console.log(deepEqual(obj1, obj3)); // false
