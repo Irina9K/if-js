56d0d9f13eed0e25e6216bfd6a2a0c187863901f
Irina Kulai <87722296+Irina9K@users.noreply.github.com>
2022-07-06 17:35:07 +0300

    added task №15

---
+++ src/js/lesson-15.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  fetch('https://fe-student-api.herokuapp.com/api/file', {
+    method: 'POST',
+    body: new FormData(form)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
